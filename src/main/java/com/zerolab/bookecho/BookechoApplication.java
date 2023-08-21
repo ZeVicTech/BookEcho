6825b63e8a336de81c8f827c102fbc7ed93204d7
Zero <kys980501@gmail.com>
2023-08-21 19:47:06 +0900

    Initial: 프로젝트 초기화

---
+++ src/main/java/com/zerolab/bookecho/BookechoApplication.java
@@ -0,0 +1,13 @@
+package com.zerolab.bookecho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookechoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookechoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zerolab/bookecho/BookechoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerolab.bookecho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookechoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
